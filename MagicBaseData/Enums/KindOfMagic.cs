948e929f208c5c589677f2eee63fbd02d9b91498
Andrey Chvankov <whitebear134@gmail.com>
2021-05-03 21:51:45 +0300

    Ohh

---
+++ MagicBaseData/Enums/KindOfMagic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MagicBaseData.Enums
+{
+    public enum KindOfMagic
+    {
+        Fire,
+        Water,
+        Air,
+        Land,
+        Lightning
+    }
+}
